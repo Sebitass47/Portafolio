31ae8acc97023a03d30775ada857ec27318bcce9
Sebitass47 <smdlr47@hotmail.com>
2022-04-12 12:59:08 -0500

    Animaciones agregadas, agregar iconos

--- web/static/web/js/principal.js
+++ web/static/web/js/principal.js
@@ -1,5 +1,27 @@
-up = () =>{
-    window.scrollTo({top: 0,
-        behavior: 'smooth'
-    });
-}
+document.addEventListener('DOMContentLoaded', function(){
+
+    up = () =>{
+        window.scrollTo({top: 0,
+            behavior: 'smooth'
+        });
+    }
+    
+    let animado = document.querySelectorAll('.animado');
+    
+    mostrarScroll = () =>{
+       
+        let scrollTop = document.documentElement.scrollTop;
+    
+        for (var i = 0; i < animado.length; i++) {
+            let alturaAnimado = animado[i].offsetTop;
+            if(scrollTop > alturaAnimado - 700 ){
+                animado[i].style.opacity = 1;
+                animado[i].classList.add('mostrarArriba');
+            }
+        }
+    
+    }
+
+
+    window.addEventListener('scroll', mostrarScroll);
+});
